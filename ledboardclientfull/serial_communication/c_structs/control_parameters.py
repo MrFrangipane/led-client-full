40b202661d7c632a60d991a9065e822f18503a37
MrFrangipane <contact@frangipane.me>
2025-04-01 23:07:32 +0200

    Ui improve

--- ledboardclientfull/serial_communication/c_structs/control_parameters.py
+++ ledboardclientfull/serial_communication/c_structs/control_parameters.py
@@ -5,11 +5,16 @@ from pythonarduinoserial.types import *
 
 @dataclass
 class ControlParametersStruct:
-    speed: IntegerType() = 3
+    speed_x: IntegerType() = 0
+    speed_y: IntegerType() = 0
+    speed_z: IntegerType() = 0
+
     scale_x: IntegerType() = 200
     scale_y: IntegerType() = 200
+
     min: IntegerType() = 150
     max: IntegerType() = 1024 - 200
+
     r: IntegerType() = 0
     g: IntegerType() = 200
     b: IntegerType() = 200
--- scripts/ui-control-parameters.py
+++ scripts/ui-control-parameters.py
@@ -5,14 +5,23 @@ from ledboardclientfull.serial_communication.c_structs.control_parameters import
 
 
 class UiControlParameters:
+
+    SpeedRange = 200
+    ScaleRange = 500
+    MinMaxRange = 1024
+
     def __init__(self):
         self.board = BoardApi(serial_port='COM9')
 
     def send_to_board(self, value):
         self.board.set_control_parameters(ControlParametersStruct(
-            speed=self.speed.get(),
+            speed_x=self.speed_x.get(),
+            speed_y=self.speed_y.get(),
+            speed_z=self.speed_z.get(),
             scale_x=self.scale_x.get(),
-            scale_y=self.scale_y.get()
+            scale_y=self.scale_y.get(),
+            min=self.min.get(),
+            max=self.max.get(),
         ))
 
     def get_from_board(self):
@@ -21,9 +30,13 @@ class UiControlParameters:
             print("No parameters received !")
             return
 
-        self.speed.set(parameters.speed)
+        self.speed_x.set(parameters.speed_x)
+        self.speed_y.set(parameters.speed_y)
+        self.speed_z.set(parameters.speed_z)
         self.scale_x.set(parameters.scale_x)
         self.scale_y.set(parameters.scale_y)
+        self.min.set(parameters.min)
+        self.max.set(parameters.max)
 
     def save(self):
         self.board.save_control_parameters()
@@ -34,56 +47,118 @@ class UiControlParameters:
     def _make_ui(self):
         self.master = Tk()
 
-        self.speed = Scale(
-            self.master,
-            from_=0,
-            to=50,
+        #
+        # X
+        frame_x = Frame(self.master, padx=5, pady=5, relief=RAISED, borderwidth=2)
+        frame_x.pack(fill=BOTH, expand=True, padx=5, pady=5)
+        Label(frame_x, text="X").pack(pady=5)
+        self.speed_x = Scale(
+            frame_x,
+            from_=-UiControlParameters.SpeedRange,
+            to=UiControlParameters.SpeedRange,
             orient=HORIZONTAL,
             length=200,
-            command=self.send_to_board
+            command=self.send_to_board,
         )
-        self.speed.pack()
+        self.speed_x.pack(pady=5)
 
         self.scale_x = Scale(
-            self.master,
+            frame_x,
             from_=1,
-            to=500,
+            to=UiControlParameters.ScaleRange,
             orient=HORIZONTAL,
             length=200,
-            command=self.send_to_board
+            command=self.send_to_board,
         )
-        self.scale_x.pack()
+        self.scale_x.pack(pady=5)
+
+        #
+        # Y
+        frame_y = Frame(self.master, padx=5, pady=5, relief=RAISED, borderwidth=2)
+        frame_y.pack(fill=BOTH, expand=True, padx=5, pady=5)
+        Label(frame_y, text="Y").pack(pady=5)
+        self.speed_y = Scale(
+            frame_y,
+            from_=-UiControlParameters.SpeedRange,
+            to=UiControlParameters.SpeedRange,
+            orient=HORIZONTAL,
+            length=200,
+            command=self.send_to_board,
+        )
+        self.speed_y.pack(pady=5)
 
         self.scale_y = Scale(
-            self.master,
+            frame_y,
             from_=1,
-            to=500,
+            to=UiControlParameters.ScaleRange,
             orient=HORIZONTAL,
             length=200,
-            command=self.send_to_board
+            command=self.send_to_board,
         )
-        self.scale_y.pack()
+        self.scale_y.pack(pady=5)
 
+        #
+        # Z
+        frame_z = Frame(self.master, padx=5, pady=5, relief=RAISED, borderwidth=2)
+        frame_z.pack(fill=BOTH, expand=True, padx=5, pady=5)
+        Label(frame_z, text="Z").pack(pady=5)
+        self.speed_z = Scale(
+            frame_z,
+            from_=-UiControlParameters.SpeedRange,
+            to=UiControlParameters.SpeedRange,
+            orient=HORIZONTAL,
+            length=200,
+            command=self.send_to_board,
+        )
+        self.speed_z.pack(pady=5)
+
+        #
+        # Min / Max
+        frame_min_max = Frame(self.master, padx=5, pady=5, relief=RAISED, borderwidth=2)
+        frame_min_max.pack(fill=BOTH, expand=True, padx=5, pady=5)
+        Label(frame_min_max, text="min max").pack(pady=5)
+        self.min = Scale(
+            frame_min_max,
+            from_= 0,
+            to=UiControlParameters.MinMaxRange,
+            orient=HORIZONTAL,
+            length=200,
+            command=self.send_to_board,
+        )
+        self.min.pack(pady=5)
+
+        self.max = Scale(
+            frame_min_max,
+            from_= 0,
+            to=UiControlParameters.MinMaxRange,
+            orient=HORIZONTAL,
+            length=200,
+            command=self.send_to_board,
+        )
+        self.max.pack(pady=5)
+
+        #
+        # Buttons
         self.button_from_board = Button(
             self.master,
             text='From Board',
             command=self.get_from_board
         )
-        self.button_from_board.pack()
+        self.button_from_board.pack(pady=5)
 
         self.button_save = Button(
             self.master,
             text='Save',
             command=self.save
         )
-        self.button_save.pack()
+        self.button_save.pack(pady=5)
 
         self.button_in_bootloader_mode = Button(
             self.master,
             text='Reboot to bootloader',
             command=self.reboot_to_bootloader
         )
-        self.button_in_bootloader_mode.pack()
+        self.button_in_bootloader_mode.pack(pady=5)
 
 ui = UiControlParameters()
 ui._make_ui()
